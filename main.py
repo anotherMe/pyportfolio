39b87127d7b7d120185241b61964ac53806708ff
anotherMe <tuplanulla@gmail.com>
2025-10-20 12:07:55 +0200

    Hidden unused page

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ pages = {
         st.Page("pages/transactions_edit.py", title="Add / Edit"),
     ],
     "Other": [
-        st.Page("pages/settings.py", title="⚙️ Settings"),
+        # st.Page("pages/settings.py", title="⚙️ Settings"),
         st.Page("pages/backup.py", title="Backup", icon="💾"),
         # st.Page("pages/test.py", title="🧪 Test"),
     ]}
