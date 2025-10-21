5e33ce7859424bfff5a16be6d1c1e7928e2127ab
anotherMe <tuplanulla@gmail.com>
2025-10-21 19:54:42 +0200

    fix some left behind issues

--- pages/instruments_list.py
+++ pages/instruments_list.py
@@ -21,50 +21,36 @@ with get_session() as session, session.begin():
         st.info("No instruments found.")
         st.stop()
 
-
+    # --- Create dataframe ---
     data = []
     for inst in instruments:
         data.append({
-            "ID": inst.id,
+            # "ID": inst.id,
             "ISIN": f"https://www.justetf.com/en/etf-profile.html?isin={inst.isin}" if inst.isin else "",
             "Ticker": f"https://finance.yahoo.com/quote/{inst.ticker}" if inst.ticker else "",
             "Name": inst.name,
-            "Currency": inst.currency or "",
-            # "Details": f"/instruments_details/?instrument_id={inst.id}",
+            # "Currency": inst.currency or "",
         })
 
     df = pd.DataFrame(data)
-
-    # --- Manually create markdown columns 
-    df["Details"] = df["ID"].apply(
-        lambda x: f"[Details](/instruments_details?instrument_id={x})"
-    )
 
     # --- Configure columns ---
     column_config = {
         "ISIN": st.column_config.LinkColumn(
             help="Look up ISIN on JustETF site",
-            # display_text=":material/table_chart_view:"
             display_text=r"[?&]isin=([^&#]+)"
         ),
         "Ticker": st.column_config.LinkColumn(
             help="Lookup ticker on Yahoo Finance site",
-            # display_text=":material/table_chart_view:"
             display_text=r"/quote/([^/?#]+)"
         ),
-        "Details": st.column_config.TextColumn("Details")
-        # "Details": st.column_config.LinkColumn(
-        #     help="Click to open Instrument detail page",
-        #     display_text=":material/edit:"
-        # )
     }
 
     # --- Display table ---
     st.data_editor(
-        data=df.drop(columns=["ID"]),
-        # data=df,
-        # hide_index=True,
+        data=df,
+        hide_index=True,
         column_config=column_config,
-        disabled=True,  # read-only table
+        disabled=True,
     )
 
