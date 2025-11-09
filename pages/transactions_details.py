95f271c9b5020228fe338c501ef1fa504396c10d
anotherMe <tuplanulla@gmail.com>
2025-11-09 10:45:47 +0100

    search on transaction description

--- pages/transactions_details.py
+++ pages/transactions_details.py
@@ -1,58 +1,62 @@
 
+import logging
 import streamlit as st
 from lib.repo.accounts_repository import get_account_by_name, get_all_accounts
 from lib.database import read_from_db, get_session
 from service.utils import account_selector, to_local
 import lib.repo.transactions_repository as trans_repo
 
-
-print("Running transactions details page...")
-
 st.session_state.transaction_id = None  # Always reset selected transaction ID
-if "ok_delete_transaction" not in st.session_state:
-    st.session_state.ok_delete_transaction = False
-if "show_delete_transaction_confirmation_dialog" not in st.session_state:
-    st.session_state.show_delete_transaction_confirmation_dialog = False
+
+
+@st.dialog("Confirm transaction deletion")
+def confirm_delete_dialog(item, on_confirm):
+    st.write(f"Are you sure you want to delete transaction {item} ?")
+    col1, col2, col3 = st.columns([3,1,1])
+    with col2:
+        if st.button("✅ Yes"):
+            on_confirm()
+            st.rerun()
+    with col3:
+        if st.button("❌ No"):
+            st.rerun()
+
+def delete_transaction(item_id):
+    with get_session() as session, session.begin():
+        try:
+            trans_repo.delete_transaction(session, item_id)
+            st.success("Transaction deleted successfully")
+        except Exception:
+            logging.exception()
+            st.error("Error deleting transaction")
+
 
 st.title("💰 Transactions")
 st.subheader("Transaction Details")
 
-@st.dialog("Confirm transaction deletion")
-def confirm_delete_dialog():
-    st.write("Are you sure you want to delete this transaction ?")
-    col1, col2, col3 = st.columns([3,1,1])
-    with col2:
-        if st.button("✅ Yes"):
-            st.session_state.ok_delete_transaction = True
-            st.rerun()
-    with col3:
-        if st.button("❌ No"):
-            st.session_state.show_delete_transaction_confirmation_dialog = False
-            st.rerun()
-
 with get_session() as session, session.begin():
 
-    # --- Account selector ---
     accounts = get_all_accounts(session)
     account_selector(accounts) # Show sidebar account selector
     current_account = get_account_by_name(session, st.session_state.account)
-
     transactions = trans_repo.get_all_transactions(session, current_account)
+
 
     if not transactions:
         st.info("No transactions found.")
         st.stop()
 
     # --- Search filters ---
-    search_term = st.text_input("🔍 Search by ISIN, Ticker, or Name").strip().lower()
+    search_term = st.text_input("🔍 Search by Instrument (ISIN, Ticker, Name) or by Transaction description").strip().lower()
 
     if search_term:
         filtered_transactions = [
             trans for trans in transactions
-            if trans.trade and
-            ( search_term in (trans.trade.instrument.isin or "").lower()
-            or search_term in (trans.trade.instrument.ticker or "").lower()
-            or search_term in (trans.trade.instrument.name or "").lower() )
+            if ( trans.trade and
+                ( search_term in (trans.trade.instrument.isin or "").lower()
+                or search_term in (trans.trade.instrument.ticker or "").lower()
+                or search_term in (trans.trade.instrument.name or "").lower() ) ) 
+            or ( not trans.trade and search_term in (trans.description or "").lower())
         ]
     else:
         filtered_transactions = []
@@ -77,7 +81,7 @@ with get_session() as session, session.begin():
     if not filtered_transactions:
         st.info("No transactions found for the current search")
         st.stop()
-    
+
     # --- Transaction list ---
     for trans in filtered_transactions:
         with st.container(border=True):
@@ -88,7 +92,8 @@ with get_session() as session, session.begin():
 
             # --- Row 2 ---
             col1, col2 = st.columns([2, 1])
-            col1.markdown(f"**Instrument:** {trans.trade.instrument.name}")
+            if trans.trade:
+                col1.markdown(f"**Instrument:** {trans.trade.instrument.name}")
             col2.markdown(f"**Type:** {trans.type}")
             
             # --- Row 3 ---
@@ -99,24 +104,8 @@ with get_session() as session, session.begin():
             # --- Row 4: Button bar ---
             col1, col2, col3 = st.columns([6, 1, 1])  # last column small for button
             with col2:
-
-                if not st.session_state.ok_delete_transaction:
-                    if st.button("🗑️ Delete", key=f"delete_{trans.id}"):
-                        st.session_state.show_delete_transaction_confirmation_dialog = True
-                        st.rerun()
-                else:
-                    try:
-                        trans_repo.delete_transaction(session, trans.id)
-                        st.success("Transaction deleted successfully")
-                        st.session_state.ok_delete_transaction = False  # VERY IMPORTANT: without this, it would keep deleting ALL transactions
-                        st.session_state.show_delete_transaction_confirmation_dialog = False
-                        st.rerun()
-                    except Exception as e:
-                        st.error(f"Error deleting transaction: {e}")
-
-                if st.session_state.show_delete_transaction_confirmation_dialog:
-                    confirm_delete_dialog()
-
+                if st.button("🗑️ Delete", key=f"delete_{trans.id}"):
+                    confirm_delete_dialog(trans.id, lambda: delete_transaction(trans.id))
             with col3:
                 if st.button("✏️ Edit", key=f"edit_{trans.id}"):
                     st.session_state["transaction_id"] = trans.id
