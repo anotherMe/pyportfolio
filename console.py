587be4e2473c59da96e7209badcebaefe60e9ab8
anotherMe <tuplanulla@gmail.com>
2025-11-28 19:14:24 +0100

    load single ticker data through console

--- console.py
+++ console.py
@@ -9,17 +9,22 @@ def main():
 
     subparsers.add_parser("init-db", help="Initialize the database")
 
-    loaddata_parser = subparsers.add_parser("load-data", help="Load some Yahoo Finance data")
-    loaddata_parser.add_argument("-f", "--file", help="The file to ingest (must be Yahoo Finance JSON format)", required=True)
+    loadjson_parser = subparsers.add_parser("load-json", help="Load some Yahoo Finance data")
+    loadjson_parser.add_argument("-f", "--file", help="The file to ingest (must be Yahoo Finance JSON format)", required=True)
 
+    loadticker_parser = subparsers.add_parser("load", help="Load a single ticker from Yahoo Finance data")
+    loadticker_parser.add_argument("-t", "--ticker", help="The symbol ticker (eg: \"IEGE.MI\")")
+    loadticker_parser.add_argument("-d", "--days", help="How many days, starting from today, are we going to retrieve")
 
     args = parser.parse_args()
 
     match args.command:
         case "init-db":
             c.handle_init_db()
-        case "load-data":
-            c.handle_load_data(args)
+        case "load-json":
+            c.handle_load_json(args)
+        case "load":
+            c.handle_load_ticker(args)
 
 if __name__ == "__main__":
     main()
--- lib/console_handlers.py
+++ lib/console_handlers.py
@@ -1,7 +1,10 @@
 
+from datetime import datetime, timedelta
 import json
 import logging
-from lib.database import init_db
+from lib.database import get_session, init_db
+from lib.models import Instrument
+from service.YahooFinanceService import download_history
 from service.myYahooFinanceService import YahooSymbolParser
 from lib.repo.ohlcvs_repository import load_ohlcv_from_symbol
 from lib.repo.prices_repository import load_prices_from_symbol
@@ -13,9 +16,8 @@ logger = logging.getLogger(__name__)
 def handle_init_db():
     init_db()
 
-def handle_load_data(args):
+def handle_load_json(args):
 
-    # --- Load file and parse it into JSON data
     try:
         with open(args.file, mode="r", encoding="utf-8") as read_file:
             data = json.load(read_file)        
@@ -33,3 +35,20 @@ def handle_load_data(args):
     except Exception as ex:
         logger.error("Error while trying to load market prices / OHLCVs")
         logger.error(ex)
+
+def handle_load_ticker(args):
+
+    try:
+        with get_session() as session, session.begin():
+            instrument:Instrument = session.query(Instrument).where(Instrument.ticker==args.ticker).first()
+            success, message = download_history(instrument, datetime.now() - timedelta(days=int(args.days))) 
+            if success:
+                logger.info(message)
+            else:
+                logger.error(message)
+
+            return True, f"Symbol {instrument.ticker} parsed correctly"
+    except Exception as ex:
+        logger.error(f"Error while trying to load prices for {args.ticker}")
+        logger.error(ex)
+        return
